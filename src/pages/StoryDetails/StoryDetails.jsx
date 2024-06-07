57583f660af130efe6c9f427ebc24c1afa74e3d0
Jaman-Shah <jamanshah5400@gmail.com>
2024-06-07 12:53:14 +0600

    react share on story details page functionality finished

--- src/pages/StoryDetails/StoryDetails.jsx
+++ src/pages/StoryDetails/StoryDetails.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import useGetStories from "../../hooks/useGetStories";
 import { Link, useParams } from "react-router-dom";
 import useAxiosCommon from "../../hooks/useAxiosCommon";
 import { useQuery } from "@tanstack/react-query";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "react-share";
 
 const StoryDetails = () => {
   const { id } = useParams();
 
   const axiosCommon = useAxiosCommon();
+
+  const currentPageUrl = `${window.location.href}`;
+  console.log(currentPageUrl);
 
   // loading single story details
   const {
@@ -50,6 +58,7 @@ const StoryDetails = () => {
       <h1 className="font-thin text-center text-3xl my-2">
         A story about <span className="text-orange-500">{title}</span>
       </h1>
+
       <div className="flex justify-center gap-4">
         {photos &&
           photos.map((photo) => {
@@ -62,6 +71,7 @@ const StoryDetails = () => {
             );
           })}
       </div>
+
       <div className="flex flex-col justify-center items-center border p-8 my-8">
         <div
           className="h-20 w-20 bg-center bg-cover  rounded-2xl "
@@ -74,7 +84,21 @@ const StoryDetails = () => {
         <div className="border-t-2 border-black pt-6">
           <p className="font-thin text-xl">{story_text}</p>
         </div>
+        <div>
+          <div className="text-center my-4 font-bold">
+            <h1>Share This Story On</h1>
+          </div>
+          <div className="flex justify-center gap-4">
+            <FacebookShareButton url={currentPageUrl}>
+              <FacebookIcon />
+            </FacebookShareButton>
+            <TwitterShareButton url={currentPageUrl}>
+              <TwitterIcon />
+            </TwitterShareButton>
+          </div>
+        </div>
       </div>
+
       <div className="text-center">
         <Link to="/all-stories" className="border p-2 ">
           All Stories
