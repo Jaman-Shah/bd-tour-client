bd4efdb6b2507c318ebcecb7f1b147c48d79878d
Jaman-Shah <jamanshah5400@gmail.com>
2024-06-23 11:56:51 +0600

    scroll to top implementation finished

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../components/shared/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/shared/Footer";
+import ScrollToTop from "../components/shared/ScrollToTop";
 
 const Main = () => {
   return (
     <div>
       <Navbar />
+      <ScrollToTop />
       <div className="mt-24 px-4">
         <Outlet />
       </div>
